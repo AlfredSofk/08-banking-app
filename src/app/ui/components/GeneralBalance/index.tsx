1a0ef37e519c7d221aaf13f86f0ff9a332367eaf
Cristhian Alfredo Garzón Velasquez <cristhian.garzon@sofka.local>
2024-11-25 10:37:43 -0500

    [FIX] small css fix and card general balance

--- src/app/ui/components/GeneralBalance/index.tsx
+++ src/app/ui/components/GeneralBalance/index.tsx
@@ -6,7 +6,10 @@ export const GeneralBalance = ({ balance }: { balance: number }) => {
     return (
         <div className="general-balance">
         <h3 className="general-balance__title">Balance General</h3>
-        <FaDollarSign aria-hidden="true"/><p className="general-balance__amount" role="general-balance">{balance.toFixed(2)}</p>
+        <div className="general-balance__content">
+          <FaDollarSign aria-hidden="true" className="general-balance__amount"/>
+          <p className="general-balance__amount" role="general-balance">{balance.toFixed(2)}</p>
+        </div>
       </div>
     )
 
